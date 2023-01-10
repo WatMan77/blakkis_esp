bc95aea1f48a74cac5f2a29cf9ef28ba95add501
Daniel Kaluski <dansku110@gmail.com>
2023-01-10 16:36:30 +0200

    Sending a message to BlakkisVuohiBot is successful!

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
-console.log('ENVS?')
-//console.log(process.env.LIB_PATH)
-
 const { Client } = require('tdl')
 const { TDLib } = require('tdl-tdlib-addon')
 
 const client = new Client(new TDLib(process.env.LIB_PATH), {
   apiId: process.env.APP_ID, // Your api_id
   apiHash: process.env.APP_HASH // Your api_hash,
-
 })
 
-client.login().then(x => {
-  console.log(x)
-})
+const main = async () => {
+  await client.login()
+
+  const result = await client.invoke({
+    _: 'sendMessage',
+    chat_id: process.env.BLAKKIS_CHAT_ID,
+    input_message_content: {
+      _: 'inputMessageText',
+      text: {
+        _: 'formattedText',
+        text: '/kalja033'
+      }
+    }
+  })
+
+  console.log('Result?')
+  console.log(result)
+
+  // await client.close()
+
+}
+
+main().catch(console.error)
